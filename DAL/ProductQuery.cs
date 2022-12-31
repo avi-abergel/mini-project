e8930f009b7516a5644570ea7d39c38599ffa4f2
avi-abergel <avi.abergel@boyar.org.il>
2022-12-31 21:08:56 +0200

    31122022

---
+++ DAL/ProductQuery.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Model;
+
+namespace DAL
+{
+    public class ProductQuery
+    {
+        public delegate Dictionary<int,Product> connectNorthwind_delegate(SqlDataReader reader);
+        public static string connectionString = @"data source=localhost\SQLEXPRESS; initial catalog= Northwind; Integrated Security=SSPI;Persist Security Info=False";
+        public static void ConnectToDB(string query, connectNorthwind_delegate func)
+        {
+            //connecting to db
+             
+            using(SqlConnection connection= new SqlConnection(connectionString))
+            {
+                //string query = "select *from products";
+                using(SqlCommand command= new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        func(reader);
+                    }
+                }
+            }
+        }
+	
+       
+    }
+}
---
+++ Model/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int SupplierID { get; set; }
+        public int categoryID { get; set; }
+        public string quantityPerUnit { get; set; }
+        public SqlMoney UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public int UnitsOnOrder { get; set; }
+        public int ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
+    }
+}
